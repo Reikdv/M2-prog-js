45f2b53596ef8de9fdb4e4c6ccfa229f2d3eee5b
Reikdv <37676@ma-web.nl>
2023-12-05 14:10:55 +0100

    edit

--- 02CanvasHuisje/app.js
+++ 02CanvasHuisje/app.js
@@ -8,19 +8,18 @@ class App
             let canvas = document.getElementById("canvasId"); 
             console.log(canvas);  
             
-
+    
     let g = canvas.getContext("2d");
 
-    g.fillRect(0, 0, 10, 10);
     g.fillStyle = "purple";
     g.fillRect(0,0,canvas.width,canvas.height);
-        
 
+    g.fillStyle = "#000000";
+    g.fillRect(0,0,10,10);
+    
+    
 }
 }
-
-
-
 
 let app = new App();
 app.runApplication();   
