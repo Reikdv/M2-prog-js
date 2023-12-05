f8d2efe3c235f591f2047a6bdb321ab27f23e40d
Reikdv <37676@ma-web.nl>
2023-12-05 14:48:58 +0100

    edit

--- 02Lijnen/app.js
+++ 02Lijnen/app.js
@@ -11,12 +11,6 @@ class App
     
     let g = canvas.getContext("2d");
 
-    g.fillStyle = "purple";
-    g.fillRect(0,0,canvas.width,canvas.height);
-
-    g.fillStyle = "#000000";
-    g.fillRect(0,0,10,10);
-
     g.beginPath();
     g.moveTo(20,20)
     g.lineTo(300,300)
@@ -29,7 +23,7 @@ class App
     g.lineTo(580,20);
     g.closePath();
     g.stroke();
-    g.fill()
+    g.fill();
     
 }
 }
---
+++ 02huisje/app.js
@@ -0,0 +1,51 @@
+class App {
+    runApplication() {
+        console.log("hello world!");
+
+       
+        let canvas = document.getElementById("canvasId");
+        console.log(canvas);
+
+        
+        let g = canvas.getContext("2d");
+
+        
+        g.fillStyle = "skyblue";
+        g.fillRect(0, 0, canvas.width, canvas.height);
+
+        
+        g.fillStyle = "green";
+        g.fillRect(0, 200, canvas.width, 100);
+
+        
+        this.huisje(g);
+    }
+
+    huisje (g) {
+        
+        g.fillStyle = "yellow";
+        g.fillRect(75, 100, 150, 120);
+
+        
+        g.fillStyle = "brown";
+        g.beginPath();
+        g.moveTo(75, 100);
+        g.lineTo(150, 20);
+        g.lineTo(225, 100);
+        g.closePath();
+        g.fill();
+
+        
+        g.fillStyle = "brown";
+        g.fillRect(120, 160, 30, 60);
+
+        
+        g.fillStyle = "blue";
+        g.fillRect(90, 120, 30, 30);
+        g.fillRect(180, 120, 30, 30);
+    }
+}
+
+
+let app = new App();
+app.runApplication();
